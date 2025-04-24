a5141541a585411f4c869a96a9c8f68a10bfa5f2
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-04-24 18:20:57 +0200

    breadcrumbs: always unlink the last elements

--- app/helpers/dsfr_helper.rb
+++ app/helpers/dsfr_helper.rb
@@ -5,11 +5,15 @@ module DsfrHelper
     def render
       # rubocop:disable Rails/HelperInstanceVariable
       @context.dsfr_breadcrumbs do |component|
-        return "" if @elements.one?
+        return "" if @elements.one? or @elements.empty?
 
-        @elements.map do |element|
+        *links, last = @elements
+
+        links.map do |element|
           component.with_breadcrumb(href: element.path, label: element.name)
         end
+
+        component.with_breadcrumb(label: last.name) unless last.nil?
       end
       # rubocop:enable Rails/HelperInstanceVariable
     end
