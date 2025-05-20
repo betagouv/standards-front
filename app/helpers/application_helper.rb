a25bba9b98d53208f658ac6b06e8c962eb547e10
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-05-20 15:49:30 +0200

    menu: highlight the nav properly
    Closes #26.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def current_user?
     current_user.present?
   end
+
+  def current_path?(path)
+    # the root path ("/") sits above all the other paths so when we're
+    # comparing against it, make sure we're exactly on it otherwise it
+    # matches along with the others ("/startups" does start with "/").
+    if path == "/"
+      request.path == path
+    else
+      request.path.start_with?(path)
+    end
+  end
+
+  def add_current_attribute(path)
+    if current_path?(path)
+      { html_attributes: { "aria-current" => "page" } }
+    else
+      nil
+    end
+  end
 end
