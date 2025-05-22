52ba1fb09a344732e0d78f7d2782cca985ba5371
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-05-22 16:13:53 +0200

    features: add a tiny bit of doc

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -1,3 +1,13 @@
+# This tiny helper gathers feature-flag-looking ENV variables
+# (BETA_STANDARDS_ENABLES_<NAME>) and will subsequently answer
+# `has_enabled_feature?(:name)`.
+
+# No value coercion is performed, which means
+# `BETA_STANDARDS_ENABLES_FOO=false` will make
+# `is_feature_enabled?(:foo)` return true, whereas
+# `BETA_STANDARDS_ENABLES_FOO=` will consider it disabled, because
+# shoving your programming language beyond empty strings in a shell
+# environment is the kingdom of fools.
 module FeatureHelper
   def has_enabled_feature?(name)
     enabled_features[name].present?
