dfe470efb5f5f819bed9666fbf87034a4021f120
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-05-22 16:14:34 +0200

    views: unpollute and standardize the layout of pages
    Vibe coding the home page was great but it left a huge mess. Whilst
    cleaning up I also ended up tidying up the content pages which should
    all follow the same structure without having to retype
    
    .fr-container
      .fr-grid-row
        .fr-col-12.fr-col-md-7...
    
    etc, etc. That's the whole point of layouts, so ensure the main layout
    does it for us and tidy up the breakpoints everywhere.
    
    Also closes #25.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,31 @@
 module HomeHelper
+  COLUMN_DEFAULTS = {
+    "sm" => 12,
+    "md" => 8,
+    "lg" => 7
+  }
+
+  def standard_url(standard)
+    "https://github.com/betagouv/standards/blob/main/#{standard.category}/#{standard.id}.md"
+  end
+
+  def default_column_classes
+    COLUMN_DEFAULTS
+      .merge(column_overrides)
+      .map { |size, cols| "fr-col-#{size}-#{cols}" }
+  end
+
+  private
+
+  def column_overrides
+    overrides = content_for(:column_overrides)
+
+    return {} if !overrides
+
+    overrides
+      .split(",")
+      .map(&:strip)
+      .map { |pair| pair.split(": ").map(&:strip) }
+      .to_h
+  end
 end
