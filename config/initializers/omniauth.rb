a6d112716e5323ff88298d808f0509faefb3b475
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-05-22 16:01:59 +0200

    omniauth: simplify the credentials setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    env = ENV.fetch("BETA_STANDARDS_ENV")
+    config = Rails
+             .application
+             .credentials
+             .dig(ENV.fetch("BETA_STANDARDS_ENV"), :auth, :proconnect)
 
-    config = %i[
-      client_id
-      client_secret
-      proconnect_domain
-      redirect_uri
-      post_logout_redirect_uri
-    ].map { |key| [ key, Rails.application.credentials.dig(env, :auth, :proconnect, key) ] }.to_h
-
-    provider Omniauth::Proconnect, config
+    provider :proconnect, config
   else
     provider :developer
   end
