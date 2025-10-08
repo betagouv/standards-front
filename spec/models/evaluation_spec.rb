51f57eb97d74cc83cb94fa1debb962851fdcb937
Stéphane Maniaci <stephane.maniaci@gmail.com>
2025-10-08 14:09:02 +0200

    evaluation: remove ability to filter out categories
    We're not soft-launching with a subset of categories so all of this
    was pretty much unnecessary and I don't see why it would come back –
    for now it's a git relic.

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -44,30 +44,5 @@ RSpec.describe Evaluation, type: :model do
     it "is deserialized with Evaluation::Question" do
       expect(evaluation.questions.first).to be_a Evaluation::Question
     end
-
-    describe "filtering categories" do
-      before do
-        allow(ENV)
-          .to receive(:fetch)
-                .with("BETA_STANDARDS_SELECTED_CATEGORIES", "")
-                .and_return(selection)
-      end
-
-      context "when the BETA_STANDARDS_SELECTED_CATEGORIES env variable is set" do
-        let(:selection) { "other,foobar, category" }
-
-        it "only selects the category indicated" do
-          expect(evaluation.questions.map(&:id)).to contain_exactly "question-2"
-        end
-      end
-
-      context "when the BETA_STANDARDS_SELECTED_CATEGORIES env variable is not set" do
-        let(:selection) { "" }
-
-        it "doesn't filter the categories" do
-          expect(evaluation.questions.map(&:id)).to contain_exactly "question-1", "question-2"
-        end
-      end
-    end
   end
 end
