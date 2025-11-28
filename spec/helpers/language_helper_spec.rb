01198292761f182913c78d6984b8000a5204f18e
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-11-28 10:54:02 +0100

    spec: fix a forgotten spec

--- spec/helpers/language_helper_spec.rb
+++ spec/helpers/language_helper_spec.rb
@@ -7,23 +7,23 @@ describe LanguageHelper, type: :helper do
 
   let(:label) { "Le code produit est accessible" }
 
-  it { is_expected.to eq "Est-ce que le code produit est accessible ?" }
+  it { is_expected.to eq "Est-ce que le code produit est accessible ?" }
 
   context "when the title start with a vowel" do
     let(:label) { "ubar" }
 
-    it { is_expected.to eq "Est-ce qu'ubar ?" }
+    it { is_expected.to eq "Est-ce qu'ubar ?" }
   end
 
   context "when the title starts with an accented vowel" do
     let(:label) { "éteindre les ordinateurs" }
 
-    it { is_expected.to start_with "Est-ce qu'éteindre les ordinateurs ?" }
+    it { is_expected.to start_with "Est-ce qu'éteindre les ordinateurs ?" }
   end
 
   context "when the title ends with a dot" do
     let(:label) { "foobar." }
 
-    it { is_expected.to end_with "bar ?" }
+    it { is_expected.to end_with "bar ?" }
   end
 end
