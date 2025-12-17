c418906ef7f3b215fc122800aa04352d4ce1370e
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-12-17 11:01:33 +0100

    startup: add a phase-based scope

--- spec/models/startup_spec.rb
+++ spec/models/startup_spec.rb
@@ -25,5 +25,26 @@ RSpec.describe EspaceMembre::Startup, type: :model do
     context "when in the initial state" do
       it { is_expected.to be_in_investigation }
     end
+
+    context "when the startup evolves" do
+      before do
+        startup.latest_phase.update!(end: DateTime.now)
+        startup.phases.create!(start: DateTime.now, name: "construction")
+      end
+
+      it "updates the latest phase" do
+        expect(startup).to be_in_construction
+      end
+    end
+  end
+
+  describe "scopes" do
+    %i[investigation construction acceleration].each do |phase_name|
+      it "has a scope to identify a startup in #{phase_name}" do
+        startup = FactoryBot.create(:startup, "in_#{phase_name}".to_sym)
+
+        expect(EspaceMembre::Startup.in_phase(phase_name)).to contain_exactly(startup)
+      end
+    end
   end
 end
